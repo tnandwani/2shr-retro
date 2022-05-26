fae9a522d05e9ddbf94942c3388e7491307fb744
Tushar <33440945+tnandwani@users.noreply.github.com>
2022-05-25 22:02:35 -0700

    cleaned up about me

--- src/windows/About.jsx
+++ src/windows/About.jsx
@@ -35,7 +35,7 @@ export const About = (props) => {
 
                     <Panel>
                         <p className='p-3 text-center'>
-                            Hi there!My name is Tushar Nandwani and I am a self taught developer with my degree in finance. I used to teach programming to kids K-12 for a few years. If a 10 year old can make a website - so can you!
+                            Hi there! My name is Tushar Nandwani and I am a self taught developer with my degree in finance. I like to build things. All types of things: websites, apps, wearable tech, and even robots at one point.
                         </p>
                     </Panel>
 
@@ -46,11 +46,7 @@ export const About = (props) => {
                     </Panel>
 
 
-                    <Panel>
-                        <p className='p-3 text-center'>
-                            🌝      Check out my journey as a developer     🌚
-                        </p>
-                    </Panel>
+                   
                     <Panel style={{ width: '100%' }}>
                         <Timeline/>
                     </Panel>
